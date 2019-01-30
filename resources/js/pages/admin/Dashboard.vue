5f113a48e57c0a8755c03d81babfc57185e4723c
maivantoan0808 <toan.mv0823@gmail.com>
2019-01-30 14:03:14 +0700

    Init Project

---
+++ resources/js/pages/admin/Dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="card card-default">
+            <div class="card-header">Admin Dashboard</div>
+            <div class="card-body">
+                Welcome to Admin Dashboard
+            </div>
+        </div>
+        <div class="card card-default">
+            <div class="card-header">List all users</div>
+            <div class="card-body">
+                <userList></userList>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import userList from '../../components/user-list.vue'
+export default {
+    mounted() {
+        //
+    },
+    components: {
+        userList
+    }
+}
+
+</script>
